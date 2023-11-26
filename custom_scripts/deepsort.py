c208c0e53101c8fbd028949ea1a91220ae27b55d
SeanDanielBuddingh <aosafrica@gmail.com>
2023-11-26 23:45:54 +0900

    modified:   custom_scripts/deepsort.py

--- custom_scripts/deepsort.py
+++ custom_scripts/deepsort.py
@@ -66,7 +66,6 @@ data = dataset.load_data()
 preprocessing_pipeline(data)
 train_pipeline()(data)
 
-model.to('cuda')
 y_train = data.get_train_data(return_type="torch")[1]
 y_test = data.get_test_data(return_type="torch")[1]
 y_train = torch.cat([y_train, y_test], dim=0)
@@ -75,7 +74,7 @@ y_test = torch.argmax(y_test, 1)
 print(y_train)
 print(y_test)
 print(data.data.uns['CellFeatureGraph'])
-model.fit(graph=data.data.uns["CellFeatureGraph"].to('cuda'), labels=y_train)
+model.fit(graph=data.data.uns["CellFeatureGraph"], labels=y_train)
 train_losses = model.train_losses
 train_accuracies = model.train_accuracies
 
