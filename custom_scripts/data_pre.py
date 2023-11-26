fcb60204debc4eda2295cb7495f36d4e457de2a1
SeanDanielBuddingh <aosafrica@gmail.com>
2023-11-26 17:53:17 +0900

    modified:   custom_scripts/data_pre.py

--- custom_scripts/data_pre.py
+++ custom_scripts/data_pre.py
@@ -4,16 +4,15 @@ current_script_path = __file__
 current_dir = os.path.dirname(current_script_path)
 parent_dir = os.path.dirname(current_dir)
 sys.path.append(parent_dir)
-import dance
-from dance.datasets.singlemodality import ScDeepSortDataset
+
 os.environ['DGLBACKEND'] = 'pytorch'
 import torch
 import numpy as np
 import pandas as pd
-from pprint import pprint
+
 from gensim.models import Word2Vec
-from transformers import LongformerTokenizer, LongformerModel
-import gc
+from transformers import BertTokenizer, BertModel
+
 
 
 class data_pre():
@@ -26,11 +25,17 @@ class data_pre():
         parent_parent = os.path.dirname(parent_dir)
         data_dir_ = parent_parent+'\dance_data'
         self.path = data_dir_
-
-        #brain_x, corpus_brain, spleen_x, corpus_spleen, kidney_x, corpus_kidney, brain_y, spleen_y, kidney_y, b_train, corpus_btrain, s_train, corpus_strain, k_train, corpus_ktrain, btrain_y, strain_y, ktrain_y = self.load_data()
         
-        #self.get_w2v(brain_x, corpus_brain, spleen_x, corpus_spleen, kidney_x, corpus_kidney, brain_y, spleen_y, kidney_y,
-        #             b_train, corpus_btrain, s_train, corpus_strain, k_train, corpus_ktrain, btrain_y, strain_y, ktrain_y)
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+        brain_x, corpus_brain, spleen_x, corpus_spleen, kidney_x, corpus_kidney, brain_y, spleen_y, kidney_y, b_train, corpus_btrain, s_train, corpus_strain, k_train, corpus_ktrain, btrain_y, strain_y, ktrain_y = self.load_data()
+        
+        self.bert_embed(brain_x, corpus_brain, brain_y)
+        #self.bert_embed(spleen_x, corpus_spleen, spleen_y)
+        #self.bert_embed(spleen_x, corpus_spleen, kidney_y)
+        #self.bert_embed(b_train, corpus_btrain, btrain_y)
+        #self.bert_embed(s_train, corpus_strain, strain_y)
+        #self.bert_embed(k_train, corpus_ktrain, ktrain_y)
         
     def load_data(self):
         '''
@@ -45,31 +50,33 @@ class data_pre():
         This Block processes the Train set
         b_train, corpus_btrain, s_train, corpus_strain, k_train, corpus_ktrain, btrain_y, strain_y, ktrain_y
         '''
-        b_train = pd.concat([pd.read_csv('train/mouse/mouse_Brain753_data.csv', header=0, index_col=0),
-                             pd.read_csv('train/mouse/mouse_Brain3285_data.csv', header=0, index_col=0)],axis=1, ignore_index=True).fillna(0)
-                             #pd.read_csv('test/mouse/mouse_Brain2695_data.csv', header=0, index_col=0)], axis=1, ignore_index=True).fillna(0)
+        b_train = pd.concat([pd.read_csv(self.path+'\\train\\mouse\\mouse_Brain753_data.csv', header=0, index_col=0),
+                             pd.read_csv(self.path+'\\train\\mouse\\mouse_Brain3285_data.csv', header=0, index_col=0)],axis=1, ignore_index=True).fillna(0)
+                             #pd.read_csv('test\\mouse\\mouse_Brain2695_data.csv', header=0, index_col=0)], axis=1, ignore_index=True).fillna(0)
         b_train = b_train.apply(lambda x: x/x.sum(), axis=0)
-        b_train = b_train.apply(lambda x: x*10000, axis=0)
+        b_train = b_train.apply(lambda x: x*1e4, axis=0)
         b_train = b_train.apply(lambda x: np.log1p(x), axis=0).fillna(0)
-        btrain_y = pd.concat([pd.read_csv('train/mouse/mouse_Brain753_celltype.csv')['Cell_type'],
-                              pd.read_csv('train/mouse/mouse_Brain3285_celltype.csv')['Cell_type']], axis=0, ignore_index=True)
-        b_train.to_csv('normalized_brain.csv', index=True, header=True)
+        btrain_y = pd.concat([pd.read_csv(self.path+'\\train\\mouse\\mouse_Brain753_celltype.csv')['Cell_type'],
+                              pd.read_csv(self.path+'\\train\\mouse\\mouse_Brain3285_celltype.csv')['Cell_type']], axis=0, ignore_index=True).reset_index(drop=True)
+        #b_train.to_csv(self.path+'\\normalized_brain.csv', index=True, header=True)
         #mask = btrain_y.str.lower().str.contains('oligodendrocyte', na=False)
         #btrain_y.loc[mask] = 'oligodendrocyte'
-                              #pd.read_csv('test/mouse/mouse_Brain2695_celltype.csv')['Cell_type']], axis=0, ignore_index=True)
+                              #pd.read_csv('test\\mouse\\mouse_Brain2695_celltype.csv')['Cell_type']], axis=0, ignore_index=True)
 
         corpus_btrain = []
         for c_name in b_train:
             cell = b_train[c_name]
             sorted = cell[cell!=0].sort_values(ascending=True)
+            #print(sorted.index.tolist())
             corpus_btrain.append(sorted.index.tolist())
 
-        s_train = pd.read_csv('train/mouse/mouse_Spleen1970_data.csv', header=0, index_col=0).fillna(0)
+        s_train = pd.read_csv(self.path+'\\train\\mouse\\mouse_Spleen1970_data.csv', header=0, index_col=0).fillna(0)
+        s_train.columns = range(s_train.shape[1])
         s_train = s_train.apply(lambda x: x/x.sum(), axis=0)
-        s_train = s_train.apply(lambda x: x*10000, axis=0)
+        s_train = s_train.apply(lambda x: x*1e4, axis=0)
         s_train = s_train.apply(lambda x: np.log1p(x), axis=0).fillna(0)
-        strain_y = pd.read_csv('train/mouse/mouse_Spleen1970_celltype.csv')['Cell_type']
-        s_train.to_csv('normalized_spleen.csv', index=True, header=True)
+        strain_y = pd.read_csv(self.path+'\\train\\mouse\\mouse_Spleen1970_celltype.csv')['Cell_type'].reset_index(drop=True)
+        #s_train.to_csv(self.path+'\\normalized_spleen.csv', index=True, header=True)
 
 
         corpus_strain = []
@@ -78,12 +85,13 @@ class data_pre():
             sorted = cell[cell!=0].sort_values(ascending=False)
             corpus_strain.append(sorted.index.tolist())
 
-        k_train = pd.read_csv('train/mouse/mouse_Kidney4682_data.csv', header=0, index_col=0).fillna(0)
+        k_train = pd.read_csv(self.path+'\\train\\mouse\\mouse_Kidney4682_data.csv', header=0, index_col=0).fillna(0)
+        k_train.columns = range(k_train.shape[1])
         k_train = k_train.apply(lambda x: x/x.sum(), axis=0)
-        k_train = k_train.apply(lambda x: x*10000, axis=0)
+        k_train = k_train.apply(lambda x: x*1e4, axis=0)
         k_train = k_train.apply(lambda x: np.log1p(x), axis=0).fillna(0)
-        ktrain_y = pd.read_csv('train/mouse/mouse_Kidney4682_celltype.csv')['Cell_type']
-        k_train.to_csv('normalized_kidney.csv', index=True, header=True)
+        ktrain_y = pd.read_csv(self.path+'\\train\\mouse\\mouse_Kidney4682_celltype.csv')['Cell_type'].reset_index(drop=True)
+        #k_train.to_csv(self.path+'\\normalized_kidney.csv', index=True, header=True)
 
 
         corpus_ktrain = []
@@ -97,12 +105,12 @@ class data_pre():
         This Block processes the Test set
         brain_x, corpus_brain, spleen_x, corpus_spleen, kidney_x, corpus_kidney, brain_y, spleen_y, kidney_y
         '''
-        brain_x = pd.read_csv('test/mouse/mouse_Brain2695_data.csv', header=0, index_col=0)
+        brain_x = pd.read_csv(self.path+'\\test\\mouse\\mouse_Brain2695_data.csv', header=0, index_col=0).fillna(0)
         brain_x.columns = range(len(brain_x.columns))
         brain_x = brain_x.apply(lambda x: x/x.sum(), axis=0)
-        brain_x = brain_x.apply(lambda x: x*10000, axis=0)
-        brain_x = brain_x.apply(lambda x: np.log1p(x), axis=0)
-        brain_y = pd.read_csv('test/mouse/mouse_Brain2695_celltype.csv')['Cell_type'].reset_index(drop=True)
+        brain_x = brain_x.apply(lambda x: x*1e4, axis=0)
+        brain_x = brain_x.apply(lambda x: np.log1p(x), axis=0).fillna(0)
+        brain_y = pd.read_csv(self.path+'\\test\\mouse\\mouse_Brain2695_celltype.csv')['Cell_type'].reset_index(drop=True)
 
         corpus_brain = []
         for c_name in brain_x:
@@ -110,12 +118,12 @@ class data_pre():
             sorted = cell[cell!=0].sort_values(ascending=True)
             corpus_brain.append(sorted.index.tolist())
    
-        spleen_x = pd.read_csv('test/mouse/mouse_Spleen1759_data.csv', header=0, index_col=0)
+        spleen_x = pd.read_csv(self.path+'\\test\\mouse\\mouse_Spleen1759_data.csv', header=0, index_col=0).fillna(0)
         spleen_x.columns = range(len(spleen_x.columns))
         spleen_x = spleen_x.apply(lambda x: x/x.sum(), axis=0)
-        spleen_x = spleen_x.apply(lambda x: x*10000, axis=0)
-        spleen_x = spleen_x.apply(lambda x: np.log1p(x), axis=0)
-        spleen_y = pd.read_csv('test/mouse/mouse_Spleen1759_celltype.csv')['Cell_type'].reset_index(drop=True)
+        spleen_x = spleen_x.apply(lambda x: x*1e4, axis=0)
+        spleen_x = spleen_x.apply(lambda x: np.log1p(x), axis=0).fillna(0)
+        spleen_y = pd.read_csv(self.path+'\\test\\mouse\\mouse_Spleen1759_celltype.csv')['Cell_type'].reset_index(drop=True)
 
         corpus_spleen = []
         for c_name in spleen_x:
@@ -124,12 +132,12 @@ class data_pre():
             corpus_spleen.append(sorted.index.tolist())
 
 
-        kidney_x = pd.read_csv('test/mouse/mouse_Kidney203_data.csv', header=0, index_col=0)
+        kidney_x = pd.read_csv(self.path+'\\test\\mouse\\mouse_Kidney203_data.csv', header=0, index_col=0).fillna(0)
         kidney_x.columns = range(len(kidney_x.columns))
         kidney_x = kidney_x.apply(lambda x: x/x.sum(), axis=0)
-        kidney_x = kidney_x.apply(lambda x: x*10000, axis=0)
-        kidney_x = kidney_x.apply(lambda x: np.log1p(x), axis=0)
-        kidney_y = pd.read_csv('test/mouse/mouse_Kidney203_celltype.csv')['Cell_type'].reset_index(drop=True)
+        kidney_x = kidney_x.apply(lambda x: x*1e4, axis=0)
+        kidney_x = kidney_x.apply(lambda x: np.log1p(x), axis=0).fillna(0)
+        kidney_y = pd.read_csv(self.path+'\\test\\mouse\\mouse_Kidney203_celltype.csv')['Cell_type'].reset_index(drop=True)
 
         corpus_kidney = []
         for c_name in kidney_x:
@@ -153,10 +161,10 @@ class data_pre():
             except KeyError:
                 b_matrix[i] = np.zeros(b_w2v.vector_size)
         b_cells = np.dot(b_train.T.values, b_matrix)
-        np.savetxt('b_train.csv', b_cells, delimiter=',')
+        np.savetxt(self.path+'\\b_train.csv', b_cells, delimiter=',')
         brain_genes = pd.DataFrame(b_matrix)
         brain_genes.index = b_train.index 
-        brain_genes.to_csv('brain_w2v_genes.csv', index=True, header=False)
+        brain_genes.to_csv(self.path+'\\brain_w2v_genes.csv', index=True, header=False)
 
         s_w2v = self.w2v_embed(corpus_strain).wv
         s_matrix = np.zeros((len(s_train), s_w2v.vector_size))
@@ -166,10 +174,10 @@ class data_pre():
             except KeyError:
                 s_matrix[i] = np.zeros(s_w2v.vector_size)
         s_cells = np.dot(s_train.T.values, s_matrix)
-        np.savetxt('s_train.csv', s_cells, delimiter=',')
+        np.savetxt(self.path+'\\s_train.csv', s_cells, delimiter=',')
         spleen_genes = pd.DataFrame(s_matrix)
         spleen_genes.index = s_train.index
-        spleen_genes.to_csv('spleen_w2v_genes.csv', index=True, header=False)
+        spleen_genes.to_csv(self.path+'\\spleen_w2v_genes.csv', index=True, header=False)
 
         k_w2v = self.w2v_embed(corpus_ktrain).wv
         k_matrix = np.zeros((len(k_train), k_w2v.vector_size))
@@ -179,19 +187,19 @@ class data_pre():
             except KeyError:
                 k_matrix[i] = np.zeros(k_w2v.vector_size)
         k_cells = np.dot(k_train.T.values, k_matrix)
-        np.savetxt('k_train.csv', k_cells, delimiter=',')
+        np.savetxt(self.path+'\\k_train.csv', k_cells, delimiter=',')
         kidney_genes = pd.DataFrame(k_matrix)
         kidney_genes.index = k_train.index
-        kidney_genes.to_csv('kidney_w2v_genes.csv', index=True, header=False)
+        kidney_genes.to_csv(self.path+'\\kidney_w2v_genes.csv', index=True, header=False)
 
         y_all = pd.Series()
         embeddings = np.vstack([b_cells, s_cells, k_cells])
-        np.savetxt('train_embeddings.csv', embeddings, delimiter=',')
+        np.savetxt(self.path+'\\train_embeddings.csv', embeddings, delimiter=',')
         y_all = pd.concat([btrain_y, strain_y, ktrain_y])
-        y_all.to_csv('y_train.csv', index=False)
-        btrain_y.to_csv('btrain_y.csv', index=False)
-        strain_y.to_csv('strain_y.csv', index=False)
-        ktrain_y.to_csv('ktrain_y.csv', index=False)
+        y_all.to_csv(self.path+'\\y_train.csv', index=False)
+        btrain_y.to_csv(self.path+'\\btrain_y.csv', index=False)
+        strain_y.to_csv(self.path+'\\strain_y.csv', index=False)
+        ktrain_y.to_csv(self.path+'\\ktrain_y.csv', index=False)
         '''
         Test Block
         '''
@@ -204,7 +212,7 @@ class data_pre():
                 brain_matrix[i] = np.zeros(brain_w2v.vector_size)
 
         brain_cells = np.dot(brain_x.T.values, brain_matrix)
-        np.savetxt('brain_cells.csv', brain_cells, delimiter=',')        
+        np.savetxt(self.path+'\\brain_cells.csv', brain_cells, delimiter=',')        
 
         spleen_w2v = self.w2v_embed(corpus_spleen).wv
         spleen_matrix = np.zeros((len(spleen_x), spleen_w2v.vector_size))
@@ -215,7 +223,7 @@ class data_pre():
                 spleen_matrix[i] = np.zeros(spleen_w2v.vector_size)
 
         spleen_cells = np.dot(spleen_x.T.values, spleen_matrix)
-        np.savetxt('spleen_cells.csv', spleen_cells, delimiter=',')
+        np.savetxt(self.path+'\\spleen_cells.csv', spleen_cells, delimiter=',')
 
         kidney_w2v = self.w2v_embed(corpus_kidney).wv
         kidney_matrix = np.zeros((len(kidney_x), kidney_w2v.vector_size))
@@ -226,24 +234,24 @@ class data_pre():
                 kidney_matrix[i] = np.zeros(kidney_w2v.vector_size)
 
         kidney_cells = np.dot(kidney_x.T.values, kidney_matrix)
-        np.savetxt('kidney_cells.csv', kidney_cells, delimiter=',')
+        np.savetxt(self.path+'\\kidney_cells.csv', kidney_cells, delimiter=',')
 
         y_all = pd.Series()
         embeddings = np.vstack([brain_cells, spleen_cells, kidney_cells])
-        np.savetxt('w2v_embeddings.csv', embeddings, delimiter=',')
+        np.savetxt(self.path+'\\w2v_embeddings.csv', embeddings, delimiter=',')
         y_all = pd.concat([brain_y, spleen_y, kidney_y])
-        y_all.to_csv('y_values.csv', index=False)
-        brain_y.to_csv('brain_y.csv', index=False)
-        spleen_y.to_csv('spleen_y.csv', index=False)
-        kidney_y.to_csv('kidney_y.csv', index=False)
+        y_all.to_csv(self.path+'\\y_values.csv', index=False)
+        brain_y.to_csv(self.path+'\\brain_y.csv', index=False)
+        spleen_y.to_csv(self.path+'\\spleen_y.csv', index=False)
+        kidney_y.to_csv(self.path+'\\kidney_y.csv', index=False)
 
     def read_w2v(self):
-        train = np.genfromtxt(self.path+'\\b_train.csv', delimiter=',')
-        genes = np.genfromtxt(self.path+'\\brain_w2v_genes.csv', delimiter=',')
-        y_train = np.loadtxt(self.path+'\\train_y.csv', delimiter=',', unpack=True, skiprows=1, dtype=str)
+        train = pd.read_csv(self.path+'\\b_train.csv', header=None)
+        genes = pd.read_csv(self.path+'\\brain_w2v_genes.csv', header=None)
+        y_train = pd.read_csv(self.path+'\\btrain_y.csv', skiprows=1, header=None, dtype=str)
         normalized = pd.read_csv(self.path+'\\normalized_brain.csv', header=0, index_col=0)
-        test = np.genfromtxt(self.path+'\\brain_cells.csv', delimiter=',')
-        y_test = np.loadtxt(self.path+'\\brain_y.csv', delimiter=',', unpack=True, skiprows=1, dtype=str)
+        test = pd.read_csv(self.path+'\\brain_cells.csv', header=None)
+        y_test = pd.read_csv(self.path+'\\brain_y.csv', skiprows=1, header=None, dtype=str)
 
         return train, y_train, genes, normalized, test, y_test
     def w2v_embed(self, corpus):
@@ -253,24 +261,16 @@ class data_pre():
         w2v_embed = Word2Vec(corpus, min_count=1)
         return w2v_embed
 
-    def longformer_embed(self, corpus):
-        '''
-        LongFormer Embeddings
-        '''
-        tokenizer = LongformerTokenizer.from_pretrained('allenai/longformer-base-4096')
-        model = LongformerModel.from_pretrained('allenai/longformer-base-4096')
-        model.eval()
-        input = [' '.join(words) for words in corpus]
-        encoded_input = tokenizer(input, padding=True, truncation=True, return_tensors='pt')
+    def bert_embed(self, x, corpus, y):
+        chunk_size = 256
+        tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+        model = BertModel.from_pretrained("bert-base-uncased").to(self.device)
+        outputs = []
+        for sentence in corpus:
+            tokenized_text = tokenizer(sentence, return_tensors="pt", truncation=True, padding=True)
+            tokenized_text = {key: value.to(self.device) for key, value in tokenized_text.items()}
+            chunk_outputs = model(**tokenized_text)
+            outputs.append(chunk_outputs)
 
-        for sentence in input:
-            tokens = tokenizer.encode(sentence, truncation=False)
-            if len(tokens) > tokenizer.model_max_length:
-                print('get truncated noob.')
-        return 0
-        with torch.no_grad():
-            outputs = model(**encoded_input)
-
-        self.bert_embed = outputs.last_hidden_state
-        print(self.bert_embed)
-
+        print(outputs)
+        return outputs
