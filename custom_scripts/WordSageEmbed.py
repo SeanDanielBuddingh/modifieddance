11488963f6f65b6fee00dbdef82016927424f892
SeanDanielBuddingh <aosafrica@gmail.com>
2024-04-16 04:30:58 +0900

    modified:   custom_scripts/WordSageEmbed.py 	modified:   custom_scripts/data_pre.py 	new file:   custom_scripts/dbscan.png 	modified:   custom_scripts/differential.py 	new file:   custom_scripts/tsne.png

--- custom_scripts/WordSageEmbed.py
+++ custom_scripts/WordSageEmbed.py
@@ -29,7 +29,7 @@ from dance.utils import set_seed
 
 
 class WordSAGE(torch.nn.Module):
-    def __init__(self, in_channels, hidden_channels, out_channels, num_classes):
+    def __init__(self, in_channels, hidden_channels, out_channels, num_classes, num_binary_targets):
         super(WordSAGE, self).__init__()
         self.seed = 42
         self.self_attention = torch.nn.MultiheadAttention(hidden_channels, num_heads=1)
@@ -40,7 +40,7 @@ class WordSAGE(torch.nn.Module):
         self.bn2 = torch.nn.BatchNorm1d(out_channels)
 
         self.linear = torch.nn.Linear(out_channels, out_channels)
-        self.bce = torch.nn.Linear(out_channels, num_classes*10)
+        self.bce = torch.nn.Linear(out_channels, num_binary_targets)
         self.ce = torch.nn.Linear(out_channels, num_classes)
 
     def forward(self, x, edge_index):
@@ -55,7 +55,7 @@ class WordSAGE(torch.nn.Module):
         h = self.bn2(h)
         x = F.leaky_relu(h)
         #decoder
-        h = self.linear(h)
+        h = self.linear(x)
         h = F.relu(h)
 
         b = self.bce(h)
@@ -64,163 +64,164 @@ class WordSAGE(torch.nn.Module):
 
         return x, b, y
 
-    def read_data(self, seed):
-        data = data_pre()
-        tissue_train, tissue_test, genes, y_values_train, y_values_test, normalized_train, normalized_test = data.read_w2v()
+def read_data(seed):
+    data = data_pre()
+    tissue_train, tissue_test, genes, y_values_train, y_values_test, normalized_train, normalized_test = data.read_w2v()
 
-        normalized_train = normalized_train.T.reset_index(drop=True)
-        normalized_test = normalized_test.T.reset_index(drop=True)
-        tissue_train = tissue_train.reset_index(drop=True)
-        tissue_test = tissue_test.reset_index(drop=True)
+    normalized_train = normalized_train.T.reset_index(drop=True)
+    normalized_test = normalized_test.T.reset_index(drop=True)
+    tissue_train = tissue_train.reset_index(drop=True)
+    tissue_test = tissue_test.reset_index(drop=True)
 
-        genes = genes.set_index(genes.iloc[:,0], drop=True)
-        genes = genes.drop(0, axis=1)
-        genes.columns = range(2500)
+    genes = genes.set_index(genes.iloc[:,0], drop=True)
+    genes = genes.drop(0, axis=1)
+    genes.columns = range(2500)
 
-        genes = genes.loc[normalized_train.columns]
+    genes = genes.loc[normalized_train.columns]
 
-        if not genes.index.equals(normalized_train.columns):
-            print('mismatch')
-            print(genes.index, normalized_train.columns)
+    if not genes.index.equals(normalized_train.columns):
+        print('mismatch')
+        print(genes.index, normalized_train.columns)
 
-        label_encoder = LabelEncoder().fit(y_values_train)
-        targets_encoded_train = pd.Series(label_encoder.transform(y_values_train))
-        targets_encoded_test = pd.Series(label_encoder.transform(y_values_test))
+    label_encoder = LabelEncoder().fit(y_values_train[0])
+    targets_encoded_train = pd.Series(label_encoder.transform(y_values_train[0]))
+    targets_encoded_test = pd.Series(label_encoder.transform(y_values_test[0]))
 
-        genemarkers = GeneMarkers()
-        full_list_train, full_list_test, _ = genemarkers.ConstructTargets(y_values_train[0], y_values_test[0], normalized_train, normalized_test)
+    genemarkers = GeneMarkers()
+    full_list_train, full_list_test, _ = genemarkers.ConstructTargets(y_values_train[0], y_values_test[0], normalized_train, normalized_test)
 
-        inputs_train, bce_targets_train_list, targets_train_list = self.mix_data(seed, tissue_train, full_list_train, targets_encoded_train)
-        inputs_test, bce_targets_test_list, targets_test_list = self.mix_data(seed, tissue_test, full_list_test, targets_encoded_test)
-        
-        train_graphs, train_nodes_list = [], []
-        for batch in inputs_train:
-            train_graph, train_nodes = self.basic_dgl_graph(batch, genes, normalized_train)
-            train_graphs.append(train_graph)
-            train_nodes_list.append(train_nodes)
-
-        test_graphs, test_nodes_list = [], []
-        for batch in inputs_test:
-            test_graph, test_nodes = self.basic_dgl_graph(batch, genes, normalized_test)
-            test_graphs.append(test_graph)
-            test_nodes_list.append(test_nodes)
-
-        return train_graphs, bce_targets_train_list, targets_train_list, test_graphs, bce_targets_test_list, targets_test_list, train_nodes_list, test_nodes_list
-
-    def basic_dgl_graph(self, train_inputs, genes, normalized):
-        num_train_nodes = len(train_inputs)
-        num_gene_nodes = len(genes)
-        G = dgl.DGLGraph()
-        
-        G.add_nodes(num_train_nodes + num_gene_nodes)
-
-        train_feature_dim = train_inputs.shape[1]
-        gene_feature_dim = genes.shape[1] 
-        max_feature_dim = max(train_feature_dim, gene_feature_dim)
-        G.ndata['features'] = torch.zeros((num_train_nodes + num_gene_nodes, max_feature_dim), dtype=torch.float)
-
-        train_features = torch.tensor(train_inputs.to_numpy(), dtype=torch.float32)
-        G.ndata['features'][:num_train_nodes] = train_features
-        
-        gene_features = torch.tensor(genes.to_numpy(), dtype=torch.float32)
-        G.ndata['features'][num_train_nodes:] = gene_features
-        
-        G.ndata['cell_id'] = torch.tensor(([-1] * num_train_nodes) + list(range(len(G.nodes())-num_train_nodes)))
-
-        edge_src = []
-        edge_dst = []
-        edge_weights = []
-        
-        for i, cell_name in enumerate(train_inputs.index):
-            vector = normalized.iloc[cell_name,:]
-            cell_index = i
-
-            for j, expression in enumerate(vector):
-                if expression == 0:
-                    continue
-                else:
-                    edge_src.append(cell_index)
-                    edge_dst.append(num_train_nodes + j)
-                    edge_weights.append(expression)
-        
-        G.add_edges(edge_src, edge_dst)
-        G.edata['weight'] = torch.tensor(edge_weights, dtype=torch.float).view(-1, 1)
-
-        del normalized
-        gc.collect()
-
-        return G, num_train_nodes
-
-    def mix_data(self, seed, inputs, bce_targets, ce_targets):
-        batch_size = 32
-        np.random.seed(seed)
-        print('Mixing Data\n')
-        # Combine inputs and targets
-        combined = pd.concat([inputs, bce_targets], axis=1)
-        print(combined.shape)
-        combined = pd.concat([combined, ce_targets], axis=1)
-        print(combined.shape)
-        # Shuffle the combined DataFrame
-        combined_shuffled = combined.sample(frac=1).reset_index(drop=True)
-
-        # Convert each row of targets to a single list
-        bce_targets_shuffled = combined_shuffled.iloc[:, 2500:-1]
-        ce_targets_shuffled = combined_shuffled.iloc[:, -1]
-
-        ls = []
-        for row in bce_targets_shuffled.iloc:
-            new = [row]
-            ls.append(new)
-        bce_targets_shuffled = ls
-
-        ls = []
-        for row in ce_targets_shuffled.iloc:
-            new = [row]
-            ls.append(new)
-        ce_targets_shuffled = ls
-
-        # Separate inputs and targets
-        inputs_shuffled = combined_shuffled.iloc[:, :2500]
-
-        inputs_shuffled = self.batch_data(inputs_shuffled, batch_size)
-        bce_targets_shuffled = self.batch_data(bce_targets_shuffled, batch_size)
-        ce_targets_shuffled = self.batch_data(ce_targets_shuffled, batch_size)
-
-        return inputs_shuffled, bce_targets_shuffled, ce_targets_shuffled
+    inputs_train, bce_targets_train_list, targets_train_list = mix_data(seed, tissue_train, full_list_train, targets_encoded_train)
+    inputs_test, bce_targets_test_list, targets_test_list = mix_data(seed, tissue_test, full_list_test, targets_encoded_test)
     
-    def batch_data(self, data, batch_size):
-        data = pd.DataFrame(data)
-        num_batches = len(data) // batch_size
-        if len(data) % batch_size != 0:
-            num_batches += 1
-        print('num batches: ', num_batches)
-        batches = []
-        for i in range(num_batches):
-            if i == num_batches-1:
-                batch = data.iloc[ i * batch_size : ]
-            else:
-                batch = data.iloc[ i * batch_size : ( i + 1 ) * batch_size ]
+    train_graphs, train_nodes_list = [], []
+    for batch in inputs_train:
+        train_graph, train_nodes = basic_dgl_graph(batch, genes, normalized_train)
+        train_graphs.append(train_graph)
+        train_nodes_list.append(train_nodes)
 
-            #batch = batch.values.tolist()
-            batches.append(batch)
-        
-        return batches
+    test_graphs, test_nodes_list = [], []
+    for batch in inputs_test:
+        test_graph, test_nodes = basic_dgl_graph(batch, genes, normalized_test)
+        test_graphs.append(test_graph)
+        test_nodes_list.append(test_nodes)
+
+    return train_graphs, bce_targets_train_list, targets_train_list, test_graphs, bce_targets_test_list, targets_test_list, train_nodes_list, test_nodes_list
+
+def basic_dgl_graph(train_inputs, genes, normalized):
+    num_train_nodes = len(train_inputs)
+    num_gene_nodes = len(genes)
+    G = dgl.DGLGraph()
+    
+    G.add_nodes(num_train_nodes + num_gene_nodes)
+
+    train_feature_dim = train_inputs.shape[1]
+    gene_feature_dim = genes.shape[1] 
+    max_feature_dim = max(train_feature_dim, gene_feature_dim)
+    G.ndata['features'] = torch.zeros((num_train_nodes + num_gene_nodes, max_feature_dim), dtype=torch.float)
+
+    train_features = torch.tensor(train_inputs.to_numpy(), dtype=torch.float32)
+    G.ndata['features'][:num_train_nodes] = train_features
+    
+    gene_features = torch.tensor(genes.to_numpy(), dtype=torch.float32)
+    G.ndata['features'][num_train_nodes:] = gene_features
+    
+    G.ndata['cell_id'] = torch.tensor(([-1] * num_train_nodes) + list(range(len(G.nodes())-num_train_nodes)))
+
+    edge_src = []
+    edge_dst = []
+    edge_weights = []
+    
+    for i, cell_name in enumerate(train_inputs.index):
+        vector = normalized.iloc[cell_name,:]
+        cell_index = i
+
+        for j, expression in enumerate(vector):
+            if expression == 0:
+                continue
+            else:
+                edge_src.append(cell_index)
+                edge_dst.append(num_train_nodes + j)
+                edge_weights.append(expression)
+    
+    G.add_edges(edge_src, edge_dst)
+    G.edata['weight'] = torch.tensor(edge_weights, dtype=torch.float).view(-1, 1)
+
+    del normalized
+    gc.collect()
+
+    return G, num_train_nodes
+
+def mix_data(seed, inputs, bce_targets, ce_targets):
+    batch_size = 32
+    np.random.seed(seed)
+    print('\nMixing Data\n')
+    # Combine inputs and targets
+    combined = pd.concat([inputs, bce_targets], axis=1)
+    print(combined.shape)
+    combined = pd.concat([combined, ce_targets], axis=1)
+    print(combined.shape,'\n')
+    # Shuffle the combined DataFrame
+    combined_shuffled = combined.sample(frac=1).reset_index(drop=True)
+
+    # Convert each row of targets to a single list
+    bce_targets_shuffled = combined_shuffled.iloc[:, 2500:-1]
+    ce_targets_shuffled = combined_shuffled.iloc[:, -1]
+
+    ls = []
+    for row in bce_targets_shuffled.iloc:
+        new = [row]
+        ls.append(new)
+    bce_targets_shuffled = ls
+
+    ls = []
+    for row in ce_targets_shuffled.iloc:
+        new = [row]
+        ls.append(new)
+    ce_targets_shuffled = ls
+
+    # Separate inputs and targets
+    inputs_shuffled = combined_shuffled.iloc[:, :2500]
+
+    inputs_shuffled = batch_data(inputs_shuffled, batch_size)
+    bce_targets_shuffled = batch_data(bce_targets_shuffled, batch_size)
+    ce_targets_shuffled = batch_data(ce_targets_shuffled, batch_size)
+
+    return inputs_shuffled, bce_targets_shuffled, ce_targets_shuffled
+
+def batch_data(data, batch_size):
+    data = pd.DataFrame(data)
+    num_batches = len(data) // batch_size
+    if len(data) % batch_size != 0:
+        num_batches += 1
+    print('num batches: ', num_batches)
+    batches = []
+    for i in range(num_batches):
+        if i == num_batches-1:
+            batch = data.iloc[ i * batch_size : ]
+        else:
+            batch = data.iloc[ i * batch_size : ( i + 1 ) * batch_size ]
+
+        #batch = batch.values.tolist()
+        batches.append(batch)
+    
+    return batches
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(device)
 seed = 42
+train_graphs, bce_targets_train_list, targets_train_list, test_graphs, bce_targets_test_list, targets_test_list, train_nodes_list, test_nodes_list = read_data(seed=seed)
 in_channels = 2500
 hidden_channels = 2500
 out_channels = 2500
 num_classes = 16
+num_binary_targets = len(bce_targets_train_list[0][0][0])
+print('\nnum_binary_targets: ', num_binary_targets)
 lr = 1e-2
 momentum = 0.9
 
 set_seed(seed)
 
-model = WordSAGE(in_channels, hidden_channels, out_channels, num_classes).to(device)
-
-train_graphs, bce_targets_train_list, targets_train_list, test_graphs, bce_targets_test_list, targets_test_list, train_nodes_list, test_nodes_list = WordSAGE.read_data(self=model, seed=seed)
+model = WordSAGE(in_channels, hidden_channels, out_channels, num_classes, num_binary_targets).to(device)
 
 bce_loss = torch.nn.BCEWithLogitsLoss()
 ce_loss = torch.nn.CrossEntropyLoss()
@@ -276,7 +277,7 @@ for epoch in range(50):
 
     bce_acc= count/bcc_denom
     if (epoch+1) % 1 == 0:
-        print(f"[UPDATE TRAIN SET] [EPOCH {epoch + 1}] BCE Loss: {bce_loss_value:.4f} | CE Loss: {ce_loss_value:.4f} | Total Loss: {loss:.4f} | BCE_Acc: {bce_acc:.4f} | CE_Acc: {ce_acc:.4f}")
+        print(f"\n[UPDATE TRAIN SET] [EPOCH {epoch + 1}] BCE Loss: {bce_loss_value:.4f} | CE Loss: {ce_loss_value:.4f} | Total Loss: {loss:.4f} | BCE_Acc: {bce_acc:.4f} | CE_Acc: {ce_acc:.4f}")
 
 features_dfs = [pd.DataFrame(tensor.numpy()) for tensor in features_list]
 targets_dfs = [pd.DataFrame(tensor.cpu().detach().numpy()) for tensor in targets_list]
--- custom_scripts/differential.py
+++ custom_scripts/differential.py
@@ -1,6 +1,13 @@
 import numpy as np
 import pandas as pd
 import scanpy as sc
+import itertools
+import matplotlib.pyplot as plt
+
+from sklearn.manifold import TSNE
+from sklearn.cluster import DBSCAN
+
+import torch
 
 import sys
 import os
@@ -19,34 +26,91 @@ class GeneMarkers():
         self.path = data_dir_
 
     def FindGeneMarkers(self, y_values_train, y_values_test, normalized_train, normalized_test):
-        
+
         obs_train = pd.DataFrame({'condition': y_values_train})
+        obs_train.index = obs_train.index.astype(str)
+
         obs_test = pd.DataFrame({'condition': y_values_test})
+        obs_test.index = obs_test.index.astype(str)
 
         adata1 = sc.AnnData(X=normalized_train, obs=obs_train)
         adata2 = sc.AnnData(X=normalized_test, obs=obs_test)
         adata = sc.concat([adata1, adata2], join="outer")
 
+        groups = adata.obs['condition'].unique()
+        num_conditions = len(groups)
+        print(f'\nThere are {num_conditions} cell-types in this dataset.\n')
+        print(groups,'\n')
+
         sc.tl.rank_genes_groups(adata, groupby='condition', method='t-test')
 
         results = adata.uns['rank_genes_groups']
-
-        groups = results['names'].dtype.names
 
         dfs = {}
         for group in groups:
             df = pd.DataFrame({
                 'names': results['names'][group],
                 'pvals': results['pvals'][group],
-                'logfoldchanges': results['logfoldchanges'][group]
             })
             df = df.sort_values('pvals', ascending=True)
             dfs[group] = df
 
-        return dfs
+        tsne = TSNE(n_components=2, random_state=0)
+        y = pd.Categorical(adata.obs['condition'])
+        y = y.codes
+        Z = tsne.fit_transform(X=adata.X, y=y)
+        plt.scatter(Z[:, 0], Z[:, 1], c=y, cmap='tab20', s=10)
+        plt.savefig('tsne.png')
+
+        dbscan = DBSCAN(eps=0.5, min_samples=5)
+        clusters = dbscan.fit_predict(Z)
+        adata.obs['dbscan']  = clusters
+        plt.scatter(Z[:, 0], Z[:, 1], c=clusters, cmap='tab20', s=10)
+        plt.savefig('dbscan.png')
+
+        cluster_conditions = {}
+        total_cells = adata.shape[0]
+        grouped = adata.obs.groupby('dbscan')
+        for cluster_label, group in grouped:
+            cluster_cells = len(group)
+    
+            if cluster_cells / total_cells < 0.1:
+                continue
+        
+            conditions = group['condition']
+            unique_conditions = conditions.unique()
+
+            condition_names = []
+            for condition in unique_conditions:
+                condition_proportion = (conditions == condition).sum() / cluster_cells
+
+                if condition_proportion >= 0.1:
+                    condition_names.append(condition)
+
+            if len(condition_names) > 1:
+                cluster_conditions[cluster_label] = condition_names
+
+        print(cluster_conditions)
+
+        group_markers = {}
+        for cluster, conditions in cluster_conditions.items():
+            adata.obs['grouping'] = adata.obs['condition'].apply(lambda x: 'group' if x in conditions else 'rest')
+            
+            sc.tl.rank_genes_groups(adata, groupby='grouping', method='t-test')
+
+            names = adata.uns['rank_genes_groups']['names']['group']
+            pvals = adata.uns['rank_genes_groups']['pvals']['group']
+
+            significant_genes = names[pvals < 0.05]
+
+            group_markers[cluster] = significant_genes
+
+        df_group = pd.DataFrame.from_dict(group_markers, orient='index').transpose()
+
+        return dfs, df_group, cluster_conditions
 
     def ConstructTargets(self, y_values_train, y_values_test, normalized_train, normalized_test):
-        dfs = self.FindGeneMarkers(y_values_train, y_values_test, normalized_train, normalized_test)
+        dfs, df_group, cluster_conditions = self.FindGeneMarkers(y_values_train, y_values_test, normalized_train, normalized_test)
         sublist_length = 10
 
         all_names = []
@@ -89,14 +153,26 @@ class GeneMarkers():
                 all_names[indices[0]] = replacement_name
  
         group_names = [all_names[i:i+sublist_length] for i in range(0, len(all_names), sublist_length)]
-        group_names_df = pd.DataFrame(group_names)
+        group_names_df = pd.DataFrame(group_names).T
 
-        unique_names = pd.unique(group_names_df.values.flatten())
-        print('\nnum_classes: ', len(unique_names))
+        # group_names_df has columns as groups and rows as gene names
+        # columns are in the same order as dfs.keys()
+        # here is where i will check the key, and add the supplementary genes
+        temp_df = pd.DataFrame(columns=group_names_df.columns)
+        for cluster, celltypes in cluster_conditions.items():
+            for celltype in celltypes:
+                series_df = df_group[cluster][:sublist_length].to_frame()
+                temp_df[list(dfs.keys()).index(celltype)] = series_df
 
+        group_names_df = pd.concat([group_names_df, temp_df], axis=0, ignore_index=True)
+
+        group_names_df.reset_index(drop=True, inplace=True)
+        unique_names = pd.unique(group_names_df.values[~pd.isna(group_names_df.values)])
+
+        # by enumerating over dfs.items(), we can get the group name and the corresponding df
         targets = {}
         for i, (group, df) in enumerate(dfs.items()):
-            group_idx = [unique_names.tolist().index(name) for name in group_names[i]]
+            group_idx = [unique_names.tolist().index(name) for name in group_names_df.iloc[:, i] if not pd.isna(name)]
             target = np.zeros(len(unique_names))
             target[group_idx] = 1
             targets[group] = target
